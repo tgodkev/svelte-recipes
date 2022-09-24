887f5c764b270f39e23ce4d7898ba33ea8d72761
Kevin <50965376+tgodkev@users.noreply.github.com>
2022-09-24 15:04:24 -0400

    supa base set up

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,1 +1,17 @@
+import {error} from '@sveltejs/kit'
+import supabase from '$lib/header/db';
+
 export const prerender = true;
+
+
+export const load = async () => {
+try{
+        const { data, error } = await supabase.from("test").select('recipes');
+       
+      return data
+}
+catch(err){
+    console.log(err)
+
+}
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,24 @@
-<script></script>
+<script>
+  import supabase from "$lib/header/db";
+  import { onMount } from "svelte";
+
+  async function signup() {
+    const { user, session, error } = await supabase.auth.signUp({
+      email: "tgodkev@gmail.com",
+      password: "12345678",
+    });
+    alert("user created");
+  }
+  /** @type {*} */
+  export let data;
+
+  console.log(data[0]);
+</script>
 
 <div>hello world</div>
+<h2>{data[0].recipes}</h2>
+
+{#each data[0].recipes as recipe}
+  <div>{recipe}</div>
+{/each}
+<button on:click={signup}>Sign up </button>
