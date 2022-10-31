fc018c9c5e6547d043fdaf2d81f3c7d10af20e26
Kevin <50965376+tgodkev@users.noreply.github.com>
2022-10-30 20:55:00 -0400

    changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <div class=" pt-32 flex flex-wrap justify-center md:flex-row ">
   {#each data.recipes as { title, description, image, id }, i}
     <div class="card  shadow-xl my-5 mx-5 image-full  md:w-1/3 md:mx-5">
-      <figure><img src={image} alt="Shoes" /></figure>
+      <figure><img src={image} alt={title} /></figure>
       <div class="card-body">
         <h2 class="card-title">{title}</h2>
         <p>{description}</p>
