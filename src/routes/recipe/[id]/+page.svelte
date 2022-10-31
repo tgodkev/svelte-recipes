82779f2674628bd826e74a14c4d34b94fedd01b6
Kevin <50965376+tgodkev@users.noreply.github.com>
2022-10-30 21:00:37 -0400

    style change

--- src/routes/recipe/[id]/+page.svelte
+++ src/routes/recipe/[id]/+page.svelte
@@ -5,7 +5,7 @@
   $: ({ recipe } = data);
 </script>
 
-<div class="flex flex-col md:flex-row pt-10">
+<div class="flex flex-col md:flex-row justify-center pt-10">
   <div class="card glass mx-2 md:mx-10  md:w-1/2">
     <figure><img src={recipe[0].image} alt="car!" /></figure>
     <div class="card-body">
@@ -13,18 +13,19 @@
       <div class="card-actions justify-end">
         <a href="/"> <button class="btn btn-primary">Home</button></a>
       </div>
+      <div class=" flex flex-col justify-center items-center px-1 pt-10 ">
+        <h2 class="text-3xl font-bold tracking-wide pb-5">Ingredients</h2>
+        <ul>
+          {recipe[0].ingredients}
+        </ul>
+
+        <h2 class="text-3xl font-bold tracking-wide pb-5 pt-10">
+          Instructions
+        </h2>
+        <ul class="pb-10">
+          {recipe[0].instructions}
+        </ul>
+      </div>
     </div>
-  </div>
-
-  <div class=" flex flex-col justify-center items-center px-5 pt-10 md:w-1/2">
-    <h2 class="text-3xl font-bold tracking-wide pb-5">Ingredients</h2>
-    <ul>
-      {recipe[0].ingredients}
-    </ul>
-
-    <h2 class="text-3xl font-bold tracking-wide pb-5 pt-10">Instructions</h2>
-    <ul class="pb-10">
-      {recipe[0].instructions}
-    </ul>
   </div>
 </div>
