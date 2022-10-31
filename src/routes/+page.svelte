2f8fee45ab7db0e517bb93e4b94c945ad61c5788
Kevin <50965376+tgodkev@users.noreply.github.com>
2022-10-30 20:43:00 -0400

    Card Changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,8 @@
 
 <div class=" pt-32 flex flex-wrap justify-center md:flex-row ">
   {#each data.recipes as { title, description, image, id }, i}
-    <div class="card glass my-3 mx-5 md:w-1/3 md:mx-10">
-      <figure><img src={image} alt="car!" /></figure>
+    <div class="card  shadow-xl my-5 mx-5 image-full  md:w-1/3 md:mx-5">
+      <figure><img src={image} alt="Shoes" /></figure>
       <div class="card-body">
         <h2 class="card-title">{title}</h2>
         <p>{description}</p>
@@ -19,5 +19,7 @@
         </div>
       </div>
     </div>
+
+    <!--test-->
   {/each}
 </div>
