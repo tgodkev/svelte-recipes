0a3c60b23939b92b402b4ccc57f2297d3de089aa
Kevin <50965376+tgodkev@users.noreply.github.com>
2022-11-22 22:41:18 -0500

    changes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,31 +3,37 @@ import supabase from "$lib/header/db";
 
 export const load = async ({cookies}) => {
 
-    //     supabase.auth.onAuthStateChange((event, session) => {
-    //     console.log( session , 'from onAuthStateChange')
-
+        supabase.auth.onAuthStateChange((event, session) => {
        
 
-
-    //     cookies.set('Newsession', session.access_token, {
-    //       path: '/',
-    //       maxAge: 60 * 60 * 24 * 7,
-    //       sameSite: 'strict',
-    //       httpOnly: true,
-    //       secure: true,
-    
-    //    });
+       setCookies(session?.access_token)
       
 
     
       
-    // });
+    });
     
+
+
+    function setCookies(id: string | null): void {
+    {
+      cookies.set('test session', id, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7,
+        sameSite: 'strict',
+        httpOnly: true,
+        secure: true,
+  
+     });
+    }
+  }
     // 5f420076-915a-411a-8cb8-d0a3bdea3241"
   return {
     session : 'worls'
   };
-}
+
+};
+
 
 
 
